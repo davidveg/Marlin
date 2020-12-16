d2430b273d998f68bff53f9f118710732072285a
Vega, David (Tester) <david.vega@sky.uk>
2020-12-16 13:41:14 +0000

    offset adjustments, margin updated

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,11 +986,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -22, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -19, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 15
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
